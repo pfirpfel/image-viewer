cbf84f7883386c94c1a51bc60f47dc564ce624a9
Michael Kuenzli <michaelkuenzli@gmail.com>
2015-01-26 15:26:23 +0100

    two different dist-versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,14 +7,53 @@ module.exports = function(grunt) {
       },
       files: ['*.js']
     },
+
+    // clean up generated stuff
+    clean: {
+      build: ['dist'],
+      bower: ['lib']
+    },
+
+    // concatenate java script files
+    concat: {
+      options: {
+        sourceMap: true
+      },
+      viewer: {
+        src: ['viewer.js'],
+        dest: 'dist/viewer.js'
+      }
+    },
+
+    // compress js code (instead of just concatenating)
+    uglify: {
+      options: {
+        mangle: true,
+        compress: true
+      },
+      uglified_build: {
+        files: {
+          'dist/viewer.min.js': [
+            'viewer.js'
+           ]
+        }
+      },
+    },
+
+    // watch code while developing and
+    // re-build if something changes
     watch: {
       files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      tasks: ['build']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('build', ['jshint', 'clean:build', 'uglify', 'concat']);
 };
